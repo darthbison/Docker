eab381b6e4e88b9b11059ac6cf7c5d13eee4c8fe
johnson_marcus <darthbison@gmail.com>
2018-07-24 08:42:08 -0400

    Updated web code

--- envirohive/web/app.py
+++ envirohive/web/app.py
@@ -1,13 +1,24 @@
-from flask import Flask
-from redis import Redis
+from flask import Flask, render_template
+import pandas as pd
+#from redis import Redis
+import urllib
+import json
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+#redis = Redis(host='redis', port=6379)
 
-@app.route('/')
-def hello():
-    count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+def retrieveData():
+    link = "http://192.168.1.121:8090/telemetry"
+    response = urllib.urlopen(link)
+    content = response.read()
+    return content 
+
+@app.route('/readings')
+def createData():
+    jsonData = json.loads(retrieveData())
+    df = pd.read_json(retrieveData())
+    columnNames = df.columns.values
+    return render_template('view.html', records=jsonData, colnames=columnNames)        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
