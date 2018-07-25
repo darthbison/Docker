a6036de5e2928e3ccd862ccd633fe3f4b977d218
darthbison <darthbison@gmail.com>
2018-07-25 10:32:26 -0400

    Update telemetry.py

--- envirohive/consolidated/telemetry.py
+++ envirohive/consolidated/telemetry.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
+from flask import Flask, render_template
 from sense_hat import SenseHat
 import os,sys
 import time
 import temperature as temp
 import humidity as hum
 import datetime  
-
-from flask import Flask
 import csv,json
 
-sense = SenseHat()
-
 app = Flask(__name__)
+
+@app.route('/readings')
+def createData():
+    jsonData = json.loads(getReading())
+    columnNames = jsonData[0] 
+    return render_template('view.html', records=jsonData, colnames=columnNames)     
 
 def hmsToSeconds(time):
     actualTime = time.split(".")
@@ -19,18 +22,15 @@ def hmsToSeconds(time):
     return 3600*h + 60*m + s
 
 def generateData(message):
-
     temperature = round(temp.getTempInFarenheit(sense),2)
     humidity = round(hum.getHumidity(sense),2)
-
-
     message += str(temperature)
     message += "," + str(humidity)
     return message
 
 def getReading():
     fieldnames=["temperature","humidity"]
-    
+   
     message = ""
     message = generateData(message) 
 
@@ -40,10 +40,6 @@ def getReading():
     out = json.dumps( [ row for row in reader ] )
     return out
 
-@app.route('/telemetry')
-def tempJSON():
-    json = getReading()
-    return json
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',port=8090, debug=True)
+   app.run(host='0.0.0.0',port=8080, debug=True)
