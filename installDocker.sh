2831305c1dc57bf6cba7dbc31062e66810816be4
Marcus Johnson <darthbison@gmail.com>
2021-02-11 16:30:56 -0500

    Remove Docker fluff

--- installDocker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -sSL https://get.docker.com | sh
-
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
